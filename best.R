bc5b49de34eb6562839d1583575eefb97165e608
Kevin Cheng <phc07.cheng@gmail.com>
2017-03-04 23:35:10 +0800

    work on 3/4
    work on 3/4

--- best.R
+++ best.R
@@ -1,17 +1,22 @@
 best <- function(state, outcome){
         
         ## Read outcome data
-        oc <- read.csv("outcome-of-care-measure.csv", colClasses = "character")
+        oc <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         
         ## Check State and Outcome is valid
-        if (! state %in% outcome$State) {print("state is not valid, try again")}
-        else if (outcome == "heart attack") {sick <- oc$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack}
-        else if (outcome == "heart failure") {sick <- oc$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure}
-        else if (outcome == "pneumonia") {sick <- outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia}
-        else {print("sickness is not valid, try again")}
-        
+        if (! state %in% oc$State) {
+                print("state is not valid, try again")
+                stop()
+                }
+        else if (outcome == "heart attack") {sick <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"}
+        else if (outcome == "heart failure") {sick <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"}
+        else if (outcome == "pneumonia") {sick <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"}
+        else {
+                print("sickness is not valid, try again")
+                stop()}
+        sick
         ## Return hospital name in that state with lowest 30-day death
-        
-        
+        r_oc <- oc[,order(oc[,sick])]
+        r_oc
 
 }
